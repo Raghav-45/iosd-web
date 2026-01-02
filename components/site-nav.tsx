ad847990e89aff15ec861382813cfd9b332391a9
Aditya <realraghavaditya@gmail.com>
2026-01-02 17:34:49 +0530

    Update site-nav.tsx

--- components/site-nav.tsx
+++ components/site-nav.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import { FloatingDock } from '@/components/ui/floating-dock'
+import { motion } from 'motion/react'
 import {
   IconCalendar,
   IconHome,
@@ -47,12 +50,22 @@ const links = [
 
 const SiteNav = ({}) => {
   return (
-    <div className="absolute bottom-8 flex w-full justify-center">
+    <motion.div
+      initial={{ y: 100 }}
+      animate={{ y: 0 }}
+      transition={{
+        type: 'spring',
+        stiffness: 180,
+        damping: 12,
+        delay: 0.2,
+      }}
+      className="absolute bottom-8 flex w-full justify-center"
+    >
       <FloatingDock
         mobileClassName="translate-y-20" // only for demo, remove for production
         items={links}
       />
-    </div>
+    </motion.div>
   )
 }
 
